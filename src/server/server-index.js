80daccf900527a3ccbd009c2486429f756970b22
Rasm832b <93643276+Rasm832b@users.noreply.github.com>
2024-03-27 11:40:00 +0100

    Html page made and getting data

--- src/server/server-index.js
+++ src/server/server-index.js
@@ -2,14 +2,29 @@ const express = require("express");
 var app = express();
 app.use(express.json());
 
+
 value = 0;
 const data = {
     "Server-type": "Central",
     "Status": "online"
 }
 
-app.get("/",function(request, res) {
+app.get("/baba", function (req, res) {
+    console.log("HAHHAHAHA")
     res.json(data);
+})
+
+app.get("/",function(request, res) {
+
+    const filename = '/Sites/test.html'
+    res.sendFile(__dirname + filename, function(err){
+        if(err){
+            console.log("Error sending file:", err)
+        }else{
+            console.log("Sent:", filename)
+        }
+    })
+    console.log('JALALALALALLALALALL')
 })
 
 app.post("/api/getdata", function(req, res) {
